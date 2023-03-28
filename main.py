f5a113dec137541bdaf332bc54c95b5b1f867126
rokHladin <rh16274@student.uni-lj.si>
2023-03-28 20:59:18 +0200

    Add robust input handling

--- main.py
+++ main.py
@@ -1,23 +1,39 @@
 import json
 import locale
-locale.setlocale(locale.LC_ALL, 'sl_SI.UTF-8')
+import os
 
-with open("data_task1.json", "r") as car_data_file:
-    car_data = json.load(car_data_file)
+locale.setlocale(locale.LC_ALL, "sl_SI.UTF-8")
 
-file = open("rich_honda_owners.txt", "w")
+file_path = "data_task2_1.json"
+try:
+    with open(file_path, "r") as car_data_file:
+        car_data = json.load(car_data_file)
 
-for person in car_data:
-    if person["car_make"] == "Honda" and float(person["bank"][1:]) > 5000:
-        bank_balance = float(person["bank"][1:])
-        file.write(
-            f'ID: {person["id"]}\n'
-            f'Name: {person["first_name"]}\n'
-            f'Surname: {person["last_name"]}\n'
-            f'Email: {person["email"]}\n'
-            f'Gender: {person["gender"]}\n'
-            f'Bank state in USD: {locale.format_string("%.2f", bank_balance, grouping=True)}\n'
-            "=====================================\n"
-        )
+    file = open("rich_honda_owners.txt", "w")
 
-file.close()
+    for person in car_data:
+        try:
+            car_make = person["car_make"].lower()
+            if car_make == "honda":
+                if person["bank"][0] == "$" and float(person["bank"][1:]) > 5000:
+                    bank_balance = float(person["bank"][1:])
+                    file.write(
+                        f'ID: {person["id"]}\n'
+                        f'Name: {person["first_name"]}\n'
+                        f'Surname: {person["last_name"]}\n'
+                        f'Email: {person["email"]}\n'
+                        f'Gender: {person["gender"]}\n'
+                        f'Bank state in USD: {locale.format_string("%.2f", bank_balance, grouping=True)}\n'
+                        "=====================================\n"
+                    )
+                else:
+                    print(
+                        "Attribute value is incomplete ($ sign missing from bank property)"
+                    )
+        except KeyError as e:
+            print(f"Record doesn't contain {e} attribute.")
+    file.close()
+except FileNotFoundError:
+    raise FileNotFoundError(f"{file_path} does not exist!")
+except json.decoder.JSONDecodeError:
+    raise ValueError(f"{file_path} has corrupt JSON structure!")
